c2d9608284ed9c6597df0dda2cef9705530ad4e7
Maximiliam Rosén <karlsson.maximiliam@gmail.com>
2020-09-14 14:24:07 +0200

    Added simple menu

---
+++ Assets/Scripts/Menu.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Menu : MonoBehaviour
+{
+    [SerializeField] private Button startButton, quit;
+    [SerializeField] private string sceneToLoad;
+    private void Start()
+    {
+        startButton.onClick.AddListener(LoadGame);
+        quit.onClick.AddListener(QuitGame);
+    }
+
+    private void LoadGame()
+    {
+        SceneManager.LoadScene(sceneToLoad);
+    }
+    
+    private void QuitGame()
+    {
+        Application.Quit();
+    }
+}
