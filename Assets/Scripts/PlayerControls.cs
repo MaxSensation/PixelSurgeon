ea9a82b8dd7add4722d62d2a5be7d3dc4a58c79f
Maximiliam Rosén <karlsson.maximiliam@gmail.com>
2020-09-17 17:05:22 +0200

    UnlimitedOrder

--- Assets/Scripts/PlayerControls.cs
+++ Assets/Scripts/PlayerControls.cs
@@ -56,26 +56,32 @@ public class PlayerControls : MonoBehaviour
         _mousePos = _camera.ScreenToWorldPoint(_mousePos);
         RaycastHit2D[] hits = Physics2D.RaycastAll(_mousePos, Vector2.zero, 0f, mask);
 
-        if (hits.Length > 0)
-        {
-            _heldObj = hits.OrderByDescending(item => item.transform.GetComponentInChildren<SpriteRenderer>()?.sortingOrder).First().transform.gameObject;
-            _heldObjSpriteRen = _heldObj.GetComponentInChildren<SpriteRenderer>();
-            _oldSortOrder = _heldObjSpriteRen.sortingOrder;
-            _heldObjSpriteRen.sortingOrder = 10;
+        if (hits.Length <= 0) return;
+        _heldObj = (
+            from
+                item in hits
+            orderby
+                item.transform.GetComponentInChildren<SpriteRenderer>()?.sortingLayerID,
+                item.transform.GetComponentInChildren<SpriteRenderer>()?.sortingOrder descending
+            select
+                item
+        ).ToArray().First().transform.gameObject;
+        _heldObjSpriteRen = _heldObj.GetComponentInChildren<SpriteRenderer>();
+        _oldSortOrder = _heldObjSpriteRen.sortingOrder;
+        _heldObjSpriteRen.sortingOrder = 10;
 
-            if (_heldObj.layer == 9) //layer 9 == Organs
-            {
-                _startPosX = _mousePos.x - _heldObj.transform.position.x;
-                _startPosY = _mousePos.y - _heldObj.transform.position.y;
-            }
-            else if (_heldObj.layer == 8) //layer 8 == Tools
-            {
-                _startPosX = 0;
-                _startPosY = 0;
-            }
-            _heldObj.transform.localScale = new Vector3(1.2f, 1.2f, 1.2f);
-            _isHolding = true;
+        if (_heldObj.layer == 9) //layer 9 == Organs
+        {
+            _startPosX = _mousePos.x - _heldObj.transform.position.x;
+            _startPosY = _mousePos.y - _heldObj.transform.position.y;
         }
+        else if (_heldObj.layer == 8) //layer 8 == Tools
+        {
+            _startPosX = 0;
+            _startPosY = 0;
+        }
+        _heldObj.transform.localScale = new Vector3(1.2f, 1.2f, 1.2f);
+        _isHolding = true;
 
     }
 
