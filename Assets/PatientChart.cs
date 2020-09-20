7918a73e90dc1be7046eacde4607696558848a77
Maximiliam Rosén <karlsson.maximiliam@gmail.com>
2020-09-21 00:38:45 +0200

    Added Fonts

---
+++ Assets/PatientChart.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using TMPro;
+using UnityEngine;
+
+public class PatientChart : MonoBehaviour
+{
+    [SerializeField] private TextMeshProUGUI _text;
+
+    private void Start()
+    {
+    }
+}
