06c56ce1dadd6fae70962983df07a18bead93f8a
Maximiliam Rosén <karlsson.maximiliam@gmail.com>
2020-09-25 17:31:55 +0200

    Cleaned some more files

--- Assets/Editor/BodyPartEditor.cs
+++ Assets/Editor/BodyPartEditor.cs
@@ -1,21 +1,19 @@
-﻿using Human;
-using UnityEditor;
+﻿using UnityEditor;
 using UnityEngine;
 using BodyPart = Human.BodyPart;
 
 namespace Editor
 {
     [CustomEditor(typeof(BodyPart))]
-    public class OrganEditor : UnityEditor.Editor 
+    public class BodyPartEditor : UnityEditor.Editor 
     {
         public override void OnInspectorGUI()
         {
             DrawDefaultInspector();
             var organ = (BodyPart)target;
-            if (GUILayout.Button("Set Position")){
-                organ.SetCorrectPosition();
-                EditorUtility.SetDirty(organ);
-            }
+            if (!GUILayout.Button("Set Position")) return;
+            organ.SetCorrectPosition();
+            EditorUtility.SetDirty(organ);
         }
     }
 }
